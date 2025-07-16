69b93da0675966a3c4f43166c2f148670767d7af
Niko <kokbisaniko@gmail.com>
2025-07-16 22:30:28 +0700

    Add bullet management and initial game loop
    Introduces bullet array and shot tracking in main.js, sets up canvas and basic draw loop with profile drop placeholder. Adds clarifying comments to bullet.js constructor parameters.

--- bullet.js
+++ bullet.js
@@ -2,11 +2,11 @@ class Bullet{
     constructor(x, y, direction, speed, acceleration, curve, ttl, sprite = null) {
         this.x = x;
         this.y = y;
-        this.direction = direction;
+        this.direction = direction; // in degrees
         this.speed = speed;
         this.acceleration = acceleration;
         this.curve = curve;
-        this.ttl = ttl;
+        this.ttl = ttl; //time to live
         this.tick = 0;
         this.sprite = sprite;
     }
--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+let arrayBullets = [];
+let shots = 0;
+let profileDropped = false;
 
+function setup() {
+    createCanvas(1280, 720);
+    angleMode(DEGREES);
+    imageMode(CENTER);
+    textAlign(CENTER, CENTER);
+
+    translate(width / 2, height / 2);
+}
+
+function draw() {
+    background(0);
+    translate(width / 2, height / 2);
+
+    //place holder for profile drop
+    if (!profileDropped) {
+        drawIntroMessage();
+        return;
+    }
+
+    handlePhases();
+
+    for (let i = arrayBullets.length - 1; i >= 0; i--) {
+        arrayBullets[i].update();
+        if (arrayBullets[i].isExpired() && !arrayBullets[i].isExpired()) {
+            arrayBullets.splice(i, 1);
+        }
+    }
+}
