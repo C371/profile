06a577d156a85726cf482f744fa4fa87f2cecaec
Niko <kokbisaniko@gmail.com>
2025-07-16 23:29:55 +0700

    Simplify bullet rendering and management logic
    Refactored Bullet class to remove sprite handling and expiration logic, now rendering bullets as ellipses directly in update(). Simplified main.js by removing unused variables and logic, and replaced arrayBullets with bullets array for direct iteration and rendering.

--- bullet.js
+++ bullet.js
@@ -11,47 +11,17 @@ class Bullet{
         this.sprite = sprite;
     }
 
-    spawn() {
-        strokeWeight(5);
-        stroke(255);
-        point(this.x, this.y);
-    }
-
     update() {
-        this.tick++;
+    this.direction += this.curve;
+    this.speed += this.acceleration;
 
-        this.direction += this.curve;
-        this.speed += this.acceleration;
+    const rad = this.direction * Math.PI / 180;
+    const dx = Math.cos(rad);
+    const dy = -Math.sin(rad);
 
-        const rad = this.direction * Math.PI / 180;
-        const dirX = Math.cos(rad);
-        const dirY = -Math.sin(rad);
+    this.x += dx * this.speed;
+    this.y += dy * this.speed;
 
-        this.x += dirX * this.speed;
-        this.y += dirY * this.speed;
-
-        const offscreen = ( this.x < -width / 2 || this.x > width / 2 || this.y < -height / 2 || this.y > height / 2);
-        if (this.tick > this.ttl || offscreen) {
-            this.markedForRemoval = true;
-            return;
-        }
-
-        this.draw();
-    }
-
-    draw() {
-        console.log("draw running");
-        if (this.sprite) {
-            imageMode(CENTER);
-            image(this.sprite, this.x, this.y);
-        } else {
-            strokeWeight(4);
-            stroke(25, 100, 255);
-            point(this.x, this.y);
-        }
-    }
-
-    isExpired() {
-        return this.markedForRemoval;
+    ellipse(this.x, this.y, 10, 10);
     }
 }
--- main.js
+++ main.js
@@ -1,34 +1,17 @@
-let arrayBullets = [];
-let shots = 0;
-let profileDropped = true;
+let bullets = [];
 
 function setup() {
-    createCanvas(1280, 720);
-    angleMode(DEGREES);
-    imageMode(CENTER);
-    textAlign(CENTER, CENTER);
-
-    translate(width / 2, height / 2);
+  createCanvas(1280, 720);
+  angleMode(DEGREES);
+  bullets.push(new Bullet(0, 0, 0, 3, 0, 0, 600)); // Right-moving bullet
 }
 
 function draw() {
-    background(0);
-    translate(width / 2, height / 2);
-
-    //place holder for profile drop
-    if (!profileDropped) {
-        drawIntroMessage();
-        return;
-    }
-
-    handlePhases();
-
-    for (let i = arrayBullets.length - 1; i >= 0; i--) {
-        arrayBullets[i].update();
-        if (arrayBullets[i].isExpired() && !arrayBullets[i].isExpired()) {
-            arrayBullets.splice(i, 1);
-        }
-    }
+  background(0);
+  translate(width / 2, height / 2);
+  for (let b of bullets) {
+    b.update();
+  }
 }
 
 function drawIntroMessage() {
